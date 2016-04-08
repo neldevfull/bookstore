0c9eab51e2850f8043b27a6bd2be44c1ff7a1f96
neldevfull <nfrugeri@gmail.com>
2016-04-08 11:55:16 +0000

    Initial use database

--- app/routes/products.js
+++ app/routes/products.js
@@ -10,11 +10,11 @@ module.exports = function(app) {
             if(err) {
                 return console.error("could not connect to postgres", err);
             }
-            client.query("SELECT * FROM products", function(err, result) {
+            client.query("SELECT * FROM products", function(err, results) {
                 if(err) {
                     return console.error("error running query", err);
                 }
-                response.send(result.rows[0]);
+                response.render("products/list", { list: results.rows });
                 client.end();
             });
         });
---
+++ database/scripts/scripts.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE products (
+id INTEGER,
+title VARCHAR(255),
+description TEXT,
+price NUMERIC(8,2)
+);
+
+SELECT * from products	
+
+INSERT INTO products VALUES
+	(1, 'Mouse', 'Mouse Preto', 100),
+	(2, 'Cooler', 'Cooler 4 motores', 1589.80),
+	(3, 'Placa de Vídeo', 'G Force', 2895.99);
+
+	
