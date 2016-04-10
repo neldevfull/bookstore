1382935e3382e67998ed17cf7570375a4a62055f
neldevfull <nfrugeri@gmail.com>
2016-04-10 22:03:31 +0000

    Use supertest

--- test/products.js
+++ test/products.js
@@ -1,22 +1,11 @@
-var http   = require("http");
-var assert = require("assert");
+var express   = require("../config/express")();
+var supertest = require("supertest")(express);
 
 describe("#products", function() {
     it("##listing json", function(done) {
-        var config = {
-            hostname: "192.168.33.11",
-            port: 3000,
-            path: "/products",
-            headers: {
-                "Accept": "application/json"
-            }
-        };
-
-        http.get(config, function(response) {
-            assert.equal(response.statusCode, 200);
-            assert.equal(response.headers["content-type"],
-                "application/json; charset=utf-8");
-            done();
-        });
+        supertest.get("/products")
+            .set("Accept", "application/json")
+            .expect("Content-type", /json/)
+            .expect(200, done);
     });
 });
