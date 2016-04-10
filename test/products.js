9b26884914b863ec32409f0d26cd2379731b04e6
neldevfull <nfrugeri@gmail.com>
2016-04-10 20:56:44 +0000

    Implemented tests asynchronous

--- test/products.js
+++ test/products.js
@@ -1,5 +1,24 @@
+var http = require("http");
+
 describe("#products", function() {
-    it("##listing json", function() {
-        console.log("TEST!");
+    it("##listing json", function(done) {
+        var config = {
+            hostname: "192.168.33.11",
+            port: 3000,
+            path: "/products",
+            headers: {
+                "Accept": "application/json"
+            }
+        };
+
+        http.get(config, function(response) {
+            response.statusCode == 200 ? console.log("Status OK") :
+                console.log("Status not ok...")
+
+            response.headers["content-type"] == "application/json; charset=utf-8" ?
+                console.log("Content-type OK") : console.log("Content-type not ok...")
+
+            done();
+        });
     });
 });
