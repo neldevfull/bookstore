42cb7ff78e91b87dc20e42ca61677111893afc31
neldevfull <nfrugeri@gmail.com>
2016-04-17 00:53:37 +0000

    Implemented test for post method

--- test/products.js
+++ test/products.js
@@ -8,4 +8,16 @@ describe("#products", function() {
             .expect("Content-type", /json/)
             .expect(200, done);
     });
+
+    it("##register new product with invalid data", function(done) {
+        supertest.post("/products")
+            .send({title: "Node.js!", description: "New book!", price: "Hey!"})
+            .expect(302, done);
+    });
+
+    it("##register new product with valid data", function(done) {
+        supertest.post("/products")
+            .send({title: "Node.js!", description: "New book!", price: 39.99})
+            .expect(302, done);
+    });
 });
