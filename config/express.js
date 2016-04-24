120503da2d60883e4e67b469bec5f7b7a50287b9
neldevfull <nfrugeri@gmail.com>
2016-04-24 21:22:53 +0000

    Created middlewares for handling errors pages

--- config/express.js
+++ config/express.js
@@ -21,5 +21,18 @@ module.exports = function() {
         .then("infra")
         .into(app);
 
+    app.use(function(req, res, next) {
+        res.status(404).render("errors/404");
+        next();
+    });
+
+    app.use(function(error, req, res, next) {
+        if(process.env.NODE_ENV === "production"){
+            res.status(500).render("errors/500");
+            return;
+        }
+        next(error);
+    });
+
     return app;
 }
